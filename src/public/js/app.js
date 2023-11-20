f3c349349ccfdd2aa41020c4807b473331a95657
paikpaik <woduf9311@gmail.com>
2023-11-20 22:16:43 +0900

    Add: webRTC video stream

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,9 @@
+/* 
+####################################
+############### Chat ###############
+####################################
+*/
+
 const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
@@ -75,3 +81,171 @@ socket.on("room_change", (rooms) => {
     roomList.append(li);
   });
 });
+
+/* 
+####################################
+############### Call ###############
+####################################
+*/
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+let roomName_v;
+let myPeerConnection;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerHTML = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerHTML = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerHTML = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerHTML = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerHTML = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+/* 
+##########################################
+############### Video Room ###############
+##########################################
+*/
+
+const welcome_v = document.getElementById("welcome_v");
+const welcome_vForm = welcome_v.querySelector("form");
+
+async function initCall() {
+  welcome_v.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+async function handleWelcomeSubmit(e) {
+  e.preventDefault();
+  const input = welcome_vForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName_v = input.value;
+  input.value = "";
+}
+
+welcome_vForm.addEventListener("submit", handleWelcomeSubmit);
+
+// socket
+socket.on("welcome_v", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("offer 보냄");
+  socket.emit("offer", offer, roomName_v);
+});
+
+socket.on("offer", async (offer) => {
+  console.log("offer 받음");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName_v);
+  console.log("answer 보냄");
+});
+
+socket.on("answer", (answer) => {
+  console.log("answer 받음");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("candidate 받음");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// webRTC
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log("candidate 보냄");
+  socket.emit("ice", data.candidate, roomName_v);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  console.log("peer한테 stream 받음");
+  peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -71,6 +71,20 @@ io.on("connection", (socket) => {
   socket.on("nickname", (nickname) => {
     socket["nickname"] = nickname;
   });
+  // video
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome_v");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
 
 httpServer.listen(3000, handleListen);
